b735b9224aa22aedf48a32d373df590ffb49395f
aideveloperindia <aideveloperindia@gmail.com>
2026-01-17 21:12:25 +0530

    Fix menu display page template syntax

--- routes/public.js
+++ routes/public.js
@@ -785,7 +785,7 @@ router.get('/:slug/redirect/:linkId', async (req, res) => {
         <head>
           <meta charset="UTF-8">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title><%= user.businessName || user.name %> - Menu</title>
+          <title>${user.businessName || user.name} - Menu</title>
           <style>
             * {
               margin: 0;
